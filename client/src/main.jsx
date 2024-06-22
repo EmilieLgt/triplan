c4eb70fd19c5e6d19fde090a8f9b8f4d55ea2a14
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-06-22 22:52:36 +0200

    creation register page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,16 @@ import "./App.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
 ]);
 
